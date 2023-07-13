f1d8b955baafa437a128c5de749e3f2f1cfbcb67
Bruce-Erlhoff <bruce.erlhoff@gmail.com>
2023-07-13 12:18:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import DigiNavbar from './DigiNavbar';
+import DigiFooter from './DigiFooter';
+import DigiHome from './DigiHome';
 
 function App() {
+  // this app is an example of converting a basic HTML app into a React app
+  // We do this by splitting the html app into components.  In this case, we choose 
+  // navbar, main (home) section, and footer.
+  // The original HTML project is in DigitalProfile directory.
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to DigiProfile</h1>
+      <DigiNavbar/>
+      <DigiHome/>
+      <DigiFooter/>
+
     </div>
   );
 }
